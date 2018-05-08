49fafabecfcc3c5c3b29dc41cb3897a6411129f5
Buuz135 <buuz135@hotmail.com>
2018-05-08 18:45:29 +0200

    Cleaned up a bit

--- src/main/java/com/buuz135/hotornot/HotOrNot.java
+++ src/main/java/com/buuz135/hotornot/HotOrNot.java
@@ -10,9 +10,6 @@ import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
@@ -26,37 +23,6 @@ public class HotOrNot {
     public static final String MOD_ID = "hotornot";
     public static final String MOD_NAME = "HotOrNot";
     public static final String VERSION = "1.0";
-
-    /**
-     * This is the instance of your mod as created by Forge. It will never be null.
-     */
-    @Mod.Instance(MOD_ID)
-    public static HotOrNot INSTANCE;
-
-    /**
-     * This is the first initialization event. Register tile entities here.
-     * The registry events below will have fired prior to entry to this method.
-     */
-    @Mod.EventHandler
-    public void preinit(FMLPreInitializationEvent event) {
-
-    }
-
-    /**
-     * This is the second initialization event. Register custom recipes
-     */
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-
-    }
-
-    /**
-     * This is the final initialization event. Register actions from other mods here
-     */
-    @Mod.EventHandler
-    public void postinit(FMLPostInitializationEvent event) {
-
-    }
 
     @Mod.EventBusSubscriber
     public static class ServerTick {

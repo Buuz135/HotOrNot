c5d71143aae01dd5447515e8d61266831c3ed2df
Buuz135 <buuz135@hotmail.com>
2018-05-08 19:06:19 +0200

    Added a tooltip to warn you what stuff is hot

--- src/main/java/com/buuz135/hotornot/HotOrNot.java
+++ src/main/java/com/buuz135/hotornot/HotOrNot.java
@@ -2,8 +2,10 @@ package com.buuz135.hotornot;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
@@ -12,6 +14,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(
         modid = HotOrNot.MOD_ID,
@@ -31,7 +34,7 @@ public class HotOrNot {
         public static void onTick(TickEvent.ServerTickEvent event) {
             if (event.phase == TickEvent.Phase.START) {
                 for (EntityPlayerMP entityPlayerMP : FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers()) {
-                    if (!entityPlayerMP.isBurning()) {
+                    if (!entityPlayerMP.isBurning() && !entityPlayerMP.isCreative()) {
                         for (ItemStack stack : entityPlayerMP.inventory.mainInventory) {
                             if (!stack.isEmpty() && stack.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null)) {
                                 IFluidHandlerItem fluidHandlerItem = stack.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
@@ -47,11 +50,30 @@ public class HotOrNot {
         }
     }
 
+    @Mod.EventBusSubscriber(value = Side.CLIENT)
+    public static class HotTooltip {
+
+        @SubscribeEvent
+        public static void onTooltip(ItemTooltipEvent event) {
+            ItemStack stack = event.getItemStack();
+            if (HotConfig.TOOLTIP && !stack.isEmpty() && stack.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null)) {
+                IFluidHandlerItem fluidHandlerItem = stack.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+                FluidStack fluidStack = fluidHandlerItem.drain(1000, false);
+                if (fluidStack != null && fluidStack.getFluid().getTemperature(fluidStack) >= HotConfig.HOT) {
+                    event.getToolTip().add(TextFormatting.RED + "Too hot to handle!");
+                }
+            }
+        }
+    }
+
     @Config(modid = MOD_ID)
     public static class HotConfig {
 
         @Config.Comment("How hot a fluid should be to start burning the player")
         public static int HOT = 1300;
+
+        @Config.Comment("If true, the items that contain hot fluid will have a tooltip that will show that they are too hot")
+        public static boolean TOOLTIP = true;
 
         @Mod.EventBusSubscriber(modid = MOD_ID)
         private static class EventHandler {

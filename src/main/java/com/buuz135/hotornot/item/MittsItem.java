0ef226f63839316475fbf639460e974d1ccf4e00
Buuz135 <buuz135@hotmail.com>
2018-12-16 02:26:26 +0100

    Added tooltip to the mitts

--- src/main/java/com/buuz135/hotornot/item/MittsItem.java
+++ src/main/java/com/buuz135/hotornot/item/MittsItem.java
@@ -1,8 +1,14 @@
 package com.buuz135.hotornot.item;
 
 import com.buuz135.hotornot.HotOrNot;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class MittsItem extends Item {
 
@@ -16,5 +22,11 @@ public class MittsItem extends Item {
     @Override
     public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
         return false;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        tooltip.add(new TextComponentTranslation("item.hotornot.mitts.tooltip").getUnformattedComponentText());
     }
 }
